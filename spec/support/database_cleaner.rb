0df661630d4fcd646e2de672443a75fffd4dd139
Stanley Tarce <stanleytarce18@gmail.com>
2022-01-08 12:26:02 +0800

    Added Database Cleaner, Factory Bot, and Faker for Rspec
    [x] Added Gemfiles Database Cleaner, Factory Bot, and Faker
    [x] Created a support folder and enable some settings in the rails_helper.rb located in specs

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.before(:example) do
+    DatabaseCleaner.clean_with(:truncation) #Truncate before running each test
+  end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
