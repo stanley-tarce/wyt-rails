41b36b987e524bdca06ab71d6aaa04077d8d7bfb
stanley-tarce <stanleytarce18@gmail.com>
2022-01-18 22:34:10 +0800

    Fixed user.rb and formatted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-    has_many :leagues, dependent: :destroy
-    validates :email, presence: true, uniqueness: true
-    has_many :sessions
+  has_many :leagues, dependent: :destroy
+  validates :email, presence: true, uniqueness: true
+  has_many :sessions
 
-    def refresh_token_from_trade_params
-      refresh
-    end
-    def refresh_token_if_expired
-      refresh
-      Session.create(user: self, token: self.access_token)
-    end
+  def refresh_token_from_trade_params
+    refresh
+  end
+
+  def refresh_token_if_expired
+    refresh
+    Session.create(user: self, token: access_token)
+  end
+
   private
-    def refresh
-    response = Yahoo::Refresh.call({'refresh_token': self.refresh_token, 'client_id': ENV['YAHOO_CLIENT_ID_STAN_V2'], 'client_secret': ENV['YAHOO_CLIENT_SECRET_STAN_V2'], 'grant_type': 'refresh_token' })
-          self.access_token = response[:data]['access_token']
-          self.expiry = Time.now.to_i + response[:data]['expires_in'].to_i
-          self.save
-    end
+
+  def refresh
+    response = Yahoo::Refresh.call({ 'refresh_token': refresh_token, 'client_id': ENV['WYT_RAILS_CONSUMER_KEY'],
+                                     'client_secret': ENV['WYT_RAILS_CONSUMER_SECRET'], 'grant_type': 'refresh_token' })
+    self.access_token = response[:data]['access_token']
+    self.expiry = Time.now.to_i + response[:data]['expires_in'].to_i
+    save
+  end
 end
