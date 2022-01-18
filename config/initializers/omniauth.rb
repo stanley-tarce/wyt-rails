134df9a7ded832f3f8085df1606e54ad5738bede
stanley-tarce <stanleytarce18@gmail.com>
2022-01-18 20:46:27 +0800

    Change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :yahoo_auth, 
     ENV['WYT_RAILS_CONSUMER_KEY'],
-    ENV['WYT_RAILS_CONSUMER_SECRET']
+    ENV['WYT_RAILS_CONSUMER_SECRET'],
+    provider_ignores_state: true
   end
 OmniAuth.config.allowed_request_methods = %i[get post]
