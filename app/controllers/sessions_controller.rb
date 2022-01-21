59c382c4ed4a247f459c02721dc1f9b81d358d8e
stanley-tarce <stanleytarce18@gmail.com>
2022-01-21 20:00:36 +0800

    Auth Callback added image

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     def callback
       if auth_hash
         full_name = "#{auth_hash['info']['first_name']} #{auth_hash['info']['last_name']}"
-        User.create_with(full_name: full_name).find_or_create_by(email: auth_hash['info']['email']).update(access_token: auth_hash['credentials']['token'], refresh_token: auth_hash['credentials']['refresh_token'], expiry: auth_hash['credentials']['expires_at'])
+        User.create_with(full_name: full_name).find_or_create_by(email: auth_hash['info']['email']).update(access_token: auth_hash['credentials']['token'], refresh_token: auth_hash['credentials']['refresh_token'], expiry: auth_hash['credentials']['expires_at'],image: auth_hash['info']['image'])
         Session.create(user:User.find_by(email: auth_hash['info']['email']), token: auth_hash['credentials']['token'])
         redirect_to "http://localhost:3000/callback?token=#{Base64.encode64(auth_hash['credentials']['token'])}" 
       end
