8dccf2328a84a928e92ffff96f67c87abb60b20f
stanley-tarce <stanleytarce18@gmail.com>
2022-01-23 17:25:45 +0800

    Troubleshooting

--- app/controllers/api/trades_controller.rb
+++ app/controllers/api/trades_controller.rb
@@ -44,7 +44,7 @@ module Api
         roster = totrade_roster[:data][:players].select {|roster| roster[:player_key] == player.player_key }[0]
         players_to_receive << { player_name: player.player_name, player_key: player.player_key, player_team_full: roster[:player_team_full], player_team_abbr: roster[:player_team_abbr], player_number: roster[:player_number], player_positions: roster[:player_positions], player_image: roster[:player_image], stats: player_stats[:data][:player_stats].select{ |stat| stat['player_key'] == player[:player_key] }[0]}
       end
-      out = { id: trade.id, user_team_name: trade.league.team_name, user_team_key: trade.league.team_key, totrade_team_name: trade.team_name, totrade_team_key: trade.team_key, players_to_send: players_to_send, players_to_receive: players_to_receive, user_other_rosterss: user_other_roster - players_to_send, totrade_other_rosters: totrade_other_roster - players_to_receive }
+      out = { id: trade.id, user_team_name: trade.league.team_name, user_team_key: trade.league.team_key, totrade_team_name: trade.team_name, totrade_team_key: trade.team_key, players_to_send: players_to_send, players_to_receive: players_to_receive, user_other_rosterss: user_other_roster.reject{|a| players_to_send.include? a}, totrade_other_rosters: totrade_other_roster.reject{|a| players_to_receive.include? a} }
       render json: out, status: :ok
       rescue ActiveRecord::RecordNotFound
         render json: {message: "Trade Not Found"}, status: 404
