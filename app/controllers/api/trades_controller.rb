670ddfa26736cdfe1c2b9f8c6c527368fa3420fd
stanley-tarce <stanleytarce18@gmail.com>
2022-01-24 11:14:57 +0800

    Troubleshoot

--- app/controllers/api/trades_controller.rb
+++ app/controllers/api/trades_controller.rb
@@ -61,7 +61,7 @@ module Api
       end
 
       out = { id: trade.id, user_team_name: trade.league.team_name, user_team_key: trade.league.team_key,
-              totrade_team_name: trade.team_name, totrade_team_key: trade.team_key, players_to_send: players_to_send, players_to_receive: players_to_receive, user_other_rosterss: user_other_roster, totrade_other_rosters: totrade_other_roster }
+              totrade_team_name: trade.team_name, totrade_team_key: trade.team_key, players_to_send: players_to_send, players_to_receive: players_to_receive, user_other_rosters: user_other_roster, totrade_other_rosters: totrade_other_roster }
       render json: out, status: :ok
     rescue ActiveRecord::RecordNotFound
       render json: { message: 'Trade Not Found' }, status: 404
