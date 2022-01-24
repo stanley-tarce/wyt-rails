9fe5ff2e0aefc93b2cd8e171fd77a306c205bace
stanley-tarce <stanleytarce18@gmail.com>
2022-01-24 11:14:39 +0800

    Troubleshoot

--- app/controllers/api/trades_controller.rb
+++ app/controllers/api/trades_controller.rb
@@ -57,7 +57,7 @@ module Api
         # puts "****************  old   #{stat4}"
         clean_stat4 = stat4.except('player_key') rescue stat4
         # puts "****************     #{clean_stat4}"
-        totrade_other_roster << { player_name: player[:player_name], player_key: player[:player_key], player_team_full:   player[:player_team_full], player_team_abbr: player[:player_team_abbr], player_number: player[:player_number], player_positions: player[:player_positions], player_image: player[:player_image], stats: stat4}
+        totrade_other_roster << { player_name: player[:player_name], player_key: player[:player_key], player_team_full:   player[:player_team_full], player_team_abbr: player[:player_team_abbr], player_number: player[:player_number], player_positions: player[:player_positions], player_image: player[:player_image], stats: clean_stat4}
       end
 
       out = { id: trade.id, user_team_name: trade.league.team_name, user_team_key: trade.league.team_key,
