cdc36a3fce61d03dda22739ba904431a5306af6c
stanley-tarce <stanleytarce18@gmail.com>
2022-01-26 12:17:00 +0800

    Added Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -82,7 +82,7 @@ class ApplicationController < ActionController::API
       return current_user_from_trade_params.refresh_token_from_trade_params
     end
     rescue NoMethodError
-      render json: { error: 'API Failed' }, status: 400
+      render json: { error: 'Error Fetching Trade Data' }, status: 400
   end
 
   def show_token_if_user
