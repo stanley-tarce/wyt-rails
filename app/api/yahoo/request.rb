4a9e3e98d0bff8663aa8da7d8a127d2e61a71510
stanley-tarce <stanleytarce18@gmail.com>
2022-01-26 11:53:20 +0800

    Change callback in show

--- app/api/yahoo/request.rb
+++ app/api/yahoo/request.rb
@@ -18,6 +18,6 @@ module Yahoo
       parsed_e = JSON.parse(e.response)
       { code: e.http_code, status: e.message, data: parsed_e['error']['description'] }
     end
-  rescue JSON::ParserError
-    { code: 500, status: 'Internal Server Error', data: 'Internal Server Error' }
+  # rescue JSON::ParserError
+  #   { code: 500, status: 'Internal Server Error', data: 'Internal Server Error' }
 end
