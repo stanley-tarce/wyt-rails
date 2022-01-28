6ea50f529e9dfdc7e0ec24fbd54f2a274761f4f0
stanley-tarce <stanleytarce18@gmail.com>
2022-01-29 00:36:53 +0800

    Added Fix for leagues without roster

--- app/controllers/api/leagues_controller.rb
+++ app/controllers/api/leagues_controller.rb
@@ -10,15 +10,16 @@ module Api
       if leagues[:data]
         teams = []
         leagues[:data][:teams].each do |team|
-          roster = Yahoo::Client.players(updated_token, team[:team_key])[:data]
-          puts roster
+          puts team[:team_key]
+          # roster = Yahoo::Client.players(updated_token, team[:team_key])[:data]
+          # puts roster
           league_key = team[:team_key].split('.')[0..2].join('.')
           league = Yahoo::Client.league(updated_token, league_key)
           current_user.leagues.create_with(team_key: team[:team_key], team_name: team[:team_name]).find_or_create_by(league_key: league_key).update(league_name: league[:data][:league][:league_name])
-          if roster.dig('players') && roster.count != 0
+          # if roster.dig('players') && roster.count != 0
             teams << { league_key: league_key, league_name: league[:data][:league][:league_name],
                       scoring_type: league[:data][:league][:scoring_type], num_teams: league[:data][:league][:num_teams], team: { team_key: team[:team_key], team_name: team[:team_name], logo_url: team[:logo_url] },roster_positions: league[:data][:league][:roster_postions] }
-          end
+          # end
         end
         render json: teams, status: :ok
       else
