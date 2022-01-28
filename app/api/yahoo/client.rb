8e10009d96d9fb614af5d829d8e01d80df644217
stanley-tarce <stanleytarce18@gmail.com>
2022-01-29 00:32:05 +0800

    Added Fix for leagues without roster

--- app/api/yahoo/client.rb
+++ app/api/yahoo/client.rb
@@ -48,7 +48,7 @@ module Yahoo
             if response[:code] == 200
                 resp_players = response[:data]['fantasy_content']['team'][1]['roster']['0']['players']
                 players = []
-       
+                puts resp_players
                 for i in (0...resp_players['count']) do
                     flattened_player_info = {}
                     resp_players[i.to_s]['player'][0].each{|j| j.each{|k,v| flattened_player_info[k] = v }}
