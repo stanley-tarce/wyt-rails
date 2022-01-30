0483add47adf0021c6192395aea7e7167fc068ef
stanley-tarce <stanleytarce18@gmail.com>
2022-01-30 12:16:16 +0800

    Added Change in Helper

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module WytRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.action_controller.include_all_helpers = false
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
