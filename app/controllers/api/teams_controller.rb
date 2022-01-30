0de683ec60048653fc16b70d9665c6daf5583550
stanley-tarce <stanleytarce18@gmail.com>
2022-01-30 12:33:01 +0800

    Added Change in Helper

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -6,6 +6,14 @@ module Api
     prepend_before_action :authenticate_user!
     append_before_action :set_response_header
     include TradeHelper
+    def teams
+      teams = Yahoo::Client.teams_in_league(updated_token, user_params[:league_key])
+      if teams[:code] != 200
+        render json: { error: 'Invalid League Key' }, status: 400
+      else
+        render json: teams[:data], status: :ok
+      end
+    end
 
     def roster_with_stats
       roster = roster_stats(user_params[:team_key], user_params[:league_key], token)
