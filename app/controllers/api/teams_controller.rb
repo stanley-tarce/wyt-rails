4443849ee455321de37c8b5d69166971702333fd
stanley-tarce <stanleytarce18@gmail.com>
2022-01-30 12:30:22 +0800

    Added Change in Helper

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -5,40 +5,48 @@ module Api
     before_action :check_token # Order
     prepend_before_action :authenticate_user!
     append_before_action :set_response_header
-    protect_from_forgery with: :null_session
-
-    def teams
-      teams = Yahoo::Client.teams_in_league(updated_token, user_params[:league_key])
-      if teams[:code] != 200
-        render json: { error: 'Invalid League Key' }, status: 400
-      else
-        render json: teams[:data], status: :ok
-      end
-    end
-
-    def stats
-      player_stats = Yahoo::Client.player_stats(updated_token, user_params[:league_key], user_params[:player_keys])
-
-      render json: player_stats
-    end
+    include TradeHelper
 
     def roster_with_stats
-      roster = Yahoo::Client.players(updated_token, user_params[:team_key])[:data][:players]
-      roster_list = [] # Whole Roster with stats
-      player_keys = [] # List of player_keys
-      roster.map do |player|
-        player_keys << player[:player_key].to_s
+      roster = roster_stats(user_params[:team_key], user_params[:league_key], token)
+      player_info = roster[:roster]
+      player_stats = roster[:stats]
+      out = []
+      player_info.each do |player|
+         stat = begin
+          player_stats.find do |stat|
+            stat['player_key'] == player[:player_key]
+          end.except('player_key')
+        rescue StandardError
+          player_stats.find do |stat|
+            stat['player_key'] == player[:player_key]
+          end
+        end
+        out << {  player_key: player[:player_key], player_name: player[:player_name], player_team_full: player[:player_team_full], player_team_abbr: player[:player_team_abbr], player_number: player[:player_number], player_positions: player[:player_positions], player_image: player[:player_image], stats: stat}
       end
-      player_stats = Yahoo::Client.player_stats(updated_token, user_params[:league_key], player_keys.join(','))
-      roster.each do |player|
-        roster_list << { player_key: player[:player_key], player_name: player[:player_name], player_team_full: player[:player_team_full], player_team_abbr: player[:player_team_abbr], player_number: player[:player_number], player_positions: player[:player_positions], player_image: player[:player_image], stats: player_stats[:data][:player_stats].select do |stat|
-                                                                                                                                                                                                                                                                                                                         stat['player_key'] == player[:player_key]
-                                                                                                                                                                                                                                                                                                                       end [0].except('player_key') }
-      end
-      rosters = { roster: roster_list }
+      rosters = { roster: out }
       render json: rosters, status: :ok
     rescue TypeError
       render json: { error: 'Unavailable to Fetch Roster' }, status: 400
     end
+
+    # def roster_with_stats
+    #   roster = Yahoo::Client.players(updated_token, user_params[:team_key])[:data][:players]
+    #   roster_list = [] # Whole Roster with stats
+    #   player_keys = [] # List of player_keys
+    #   roster.map do |player|
+    #     player_keys << player[:player_key].to_s
+    #   end
+    #   player_stats = Yahoo::Client.player_stats(updated_token, user_params[:league_key], player_keys.join(','))
+    #   roster.each do |player|
+    #     roster_list << { player_key: player[:player_key], player_name: player[:player_name], player_team_full: player[:player_team_full], player_team_abbr: player[:player_team_abbr], player_number: player[:player_number], player_positions: player[:player_positions], player_image: player[:player_image], stats: player_stats[:data][:player_stats].select do |stat|
+    #                                                                                                                                                                                                                                                                                                                      stat['player_key'] == player[:player_key]
+    #                                                                                                                                                                                                                                                                                                                    end [0].except('player_key') }
+    #   end
+    #   rosters = { roster: roster_list }
+    #   render json: rosters, status: :ok
+    # rescue TypeError
+    #   render json: { error: 'Unavailable to Fetch Roster' }, status: 400
+    # end
   end
 end
